126e87c04cbe1d60cf7aa670e9d1e320f807b8ef
Megumi Akama <kamayatu91@gmail.com>
2022-05-11 14:57:56 -0700

    Solved ex8-9

--- exercises/ex8.js
+++ exercises/ex8.js
@@ -10,10 +10,40 @@ Create a function named repeatNumbers that will return a string with each of the
 
 const repeatNumbers = function(data) {
   // Put your solution here
+  let repeatNum = 0;
+  let numOfTimes = 0;
+  let r = 0;
+  let numOfArr = 0;
+  let print = "";
+
+  numOfArr = data.length;
+ if (numOfArr === 1) {
+    repeatNum = data[0][0]; //1
+    numOfTimes = data[0][1] //10
+    while (r < numOfTimes) {
+      print += repeatNum;
+      r++;
+    }
+ } else if (numOfArr > 1) {
+   for (let i = 0; i < numOfArr; i++) { //repeat twice 0 1
+    for (let j = 0; j < 2; j++){ //repeat twice 0 1
+        // data[i][j]; //00, 01, 10, 11 = 1,2,2,3
+        repeatNum = data[i][0]; //1
+        numOfTimes = data[i][1]; //2
+       
+        while(r < numOfTimes) {
+          print += repeatNum;
+          r++;
+        }
+    }
+   }
+ }
+
+  return print;
 };
 
-console.log(repeatNumbers([[1, 10]])); // 1111111111
-console.log(repeatNumbers([[1, 2], [2, 3]])); // 11, 222
+// console.log(repeatNumbers([[1, 10]])); // 1111111111
+// console.log(repeatNumbers([[1, 2], [2, 3]])); // 11, 222
 console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]])); // 10101010, 343434343434, 9292
 
 
--- exercises/ex9.js
+++ exercises/ex9.js
@@ -8,12 +8,30 @@ Instruction
 Create a function named camelCase that will convert a string to camel case, and return the result.
 */
 
-Input
+// Input
 const camelCase = function(input) {
-  // Your code here
+  //get all words in an array
+  //make first letter capitalized except for 1st word
+  //concatanate all words without space
+  //if the string has no space, just return the value
+  let arr = [];
+  let letter = "";
+  let firstLetter = "";
+  let remainLetter = "";
+  let convertLetter = "";
+  arr = input.split(" ");
+  let lengthOfarr = arr.length;
+
+ for (let i = 1; i < lengthOfarr; i++) {
+   firstLetter = arr[i].slice(0,1);
+   letter = firstLetter.toUpperCase();
+   remainLetter = arr[i].slice(1,arr[i].length);
+   convertLetter += letter + remainLetter; 
+ }
+  return arr[0] + convertLetter;
 };
 
-console.log(camelCase("this is a string")); // thisIsAString
-console.log(camelCase("loopy cornerstone")); //loopyCornerstone
+// console.log(camelCase("this is a string")); // thisIsAString
+// console.log(camelCase("loopy cornerstone")); //loopyCornerstone
 console.log(camelCase("supercalifragalisticexpialidocious")); // supercalifragalisticexpialidocious
 
